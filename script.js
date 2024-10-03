ea3cb5f52098dbbfb566e06094459d3144cd28a1
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-10-03 19:09:45 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let firstNum;
+let secondNum;
+let operator;
+
+// #region math functions 
+function add (a, b) {
+    return a + b;
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
+// #endregion
+
+function operate(op, a, b) {
+    switch (op) {
+        case "+": 
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+}
