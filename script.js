8efc0cf827086b84907b1e3590075d91780bc781
hbar1st <35541470+hbar1st@users.noreply.github.com>
2024-10-04 14:49:45 -0400

    add basic arithmetic ability

--- script.js
+++ script.js
@@ -2,40 +2,43 @@ const clearBtn = document.querySelector('#clear');
 const resultDiv = document.querySelector('#result');
 const expressionDiv = document.querySelector('#expression');
 const numPadBtns = document.querySelectorAll('#num-pad>button');
+const opPadBtns = document.querySelectorAll("#op-pad>button");
 
-let firstNum = null;
-let secondNum = null;
+let firstNum = "";
+let secondNum = "";
 let operator = null;
-let characters = [];
+let total = null;
+let characters = []; //what people type and we display
 
 // #region math functions 
-function add (a, b) {
-    return a + b;
+function add(a) {
+    return total + a;
 }
-function subtract (a, b) {
-    return a - b;
+function subtract(a) {
+    return total - a;
 }
-function multiply (a, b) {
-    return a * b;
+function multiply(a) {
+    return total * a;
 }
-function divide (a, b) {
-    return a / b;
+function divide(a) {
+    return total / a;
 }
 // #endregion
 
 function operate(op, a, b) {
+    total = a;
     switch (op) {
-        case "+": 
-            add(a, b);
+        case "+":
+            return add(b);
             break;
         case "-":
-            subtract(a, b);
+            return subtract(b);
             break;
         case "*":
-            multiply(a, b);
+            return multiply(b);
             break;
         case "/":
-            divide(a, b);
+            return divide(b);
             break;
     }
 }
@@ -44,27 +47,96 @@ clearBtn.addEventListener("click", () => {
     expressionDiv.innerText = "";
     resultDiv.innerText = "";
     characters = [];
-    firstNum = null;
-    secondNum = null;
+    firstNum = "";
+    secondNum = "";
     operator = null;
+    total = null;
 })
 
-numPadBtns.forEach(btn => {
-    btn.addEventListener("click", (e) => {
+numPadBtns.forEach(btn =>
+    btn.addEventListener("click", e => {
         let val = e.target.textContent;
-        if ( val === "=" ) {
-            //TODO
+        if (val === "=") {
+            if (operator) {
+                evaluate();
+            }
         } else
-        if ( val === "." ) {
-            //TODO
-        } else {
-            characters.push(Number(val));
-            displayExpression(characters);
-        }
-        
-    });
-});
+            if (val === ".") {
+                //TODO
+            } else {
+                characters.push(Number(val));
 
-function displayExpression (characters) {
+                if (operator) {
+                    secondNum += val;
+                } else {
+                    firstNum += val;
+                }
+
+                displayExpression(characters);
+            }
+
+    })
+);
+
+function displayExpression(characters) {
     expressionDiv.innerText = characters.join('');
 }
+
+function displayResult(value) {
+    resultDiv.innerText = value;
+}
+
+function evaluate() {
+    const result = operate(operator, +firstNum, +secondNum);
+    total = !isNaN(result) ? result : total;
+    firstNum = total;
+    secondNum = "";
+    operator = null;
+    displayResult(total);
+}
+
+opPadBtns.forEach(btn =>
+    btn.addEventListener("click", e => {
+        if (characters.length === 0) {
+            return; // do nothing if first thing pressed is operator
+        }
+        const prevChar = characters[characters.length - 1];
+        if (isOperator(prevChar)) {
+            operator = e.target.textContent;
+            characters.pop();
+        } else {
+            if (operator) {
+                evaluate();
+            }
+            operator = e.target.textContent;
+        }
+        characters.push(e.target.textContent);
+        displayExpression(characters);
+    }));
+
+function isOperator(str) {
+    return (str === "+") || (str === "-") || (str === '*') || (str === '/');
+}
+
+/**
+ *         if (operator) {
+
+            const regex = /(\d+)([+\-*\/]{1})(\d+)/;
+            const expression = characters.slice();
+            const matches = expression.join('').match(regex);
+            total = operate(matches[2], firstNum ?? +matches[1], +matches[3]);
+            firstNumber = total;
+            displayResult(total);
+        }
+
+        if (isOperator(characters[characters.length - 1])) {
+            // the last typed value is an operator
+            // so replace the last operator with this new one
+            characters.pop();
+        }
+        operator = e.target.textContent;
+        firstNum = characters.slice();
+        characters.push(operator);
+        displayExpression(characters);
+    })
+ */
